a9caf4fbf9ad3ba15ed4760fad0b5094da33fae1
glen <glenwojnar@yahoo.com>
2022-12-21 18:46:31 -0500

    Added additional associations

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -11,7 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Review.belongsTo(models.Spot, { foreignKey: "spotId" })
+      Review.belongsTo(models.Spot, { foreignKey: "spotId" });
+      Review.belongsTo(models.User, { foreignKey: "userId" });
     }
   }
   Review.init({

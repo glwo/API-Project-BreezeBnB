54a186065a03a55f6ac92cc4c2f1923df44eab8a
glen <glenwojnar@yahoo.com>
2022-12-21 17:05:15 -0500

    implemented Get all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Spot, Review, SpotImage } = require('../../db/models');
+const { User, Spot, Review, SpotImage, sequelize } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -10,19 +10,32 @@ const router = express.Router();
 
 
 router.get("/", async (req, res) => {
-    const spots = await Spot.findAll({
-        // attributes: {
-        //     include
-        // },
-        include: [
-            {
-                model: Review
+    const spots = await Spot.findAll({ raw:true })
+
+    for(let spot of spots){
+        const avgRating = await Review.findAll({
+            where: {
+                spotId: spot.id
             },
-            {
-                model: SpotImage
-            }
-        ]
-    })
+            attributes: {
+                include: [
+                    [
+                        sequelize.fn("AVG", sequelize.col("stars")), "avgRating"
+                    ]
+                ]
+            },
+            raw: true
+        })
+        const previewImage = await SpotImage.findAll({
+            where: {
+                spotId: spot.id
+            },
+            attributes: ['url']
+        })
+        spot.avgRating = avgRating[0].avgRating;
+        spot.previewImage = previewImage[0].url
+    }
+
     return res.json({
         Spots: spots
     })
